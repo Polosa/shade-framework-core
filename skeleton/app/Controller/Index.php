233a306336438fe9ca6d1e48d5ba722e6be6f675
Polosa <i@denis-shapkin.ru>
2014-10-17 01:36:41 +0200

    Skeleton cleanup, dummy action rewritten

--- skeleton/app/Controller/Index.php
+++ skeleton/app/Controller/Index.php
@@ -16,10 +16,9 @@ class Index extends \ShadeApp\Controller
 	 */
     public function indexAction()
     {
-        $data = array(
-            'testVar' => 'testValue',
+        return $this->render(
+            array('index.phtml', 'main_layout.phtml', 'html_layout.phtml'),
+            array('pageTitle' => $this->serviceProvider()->getApp()->getAppNamespace())
         );
-
-        return $this->render(array('index.phtml', 'main_layout.phtml', 'html_layout.phtml'), $data);
     }
 }
